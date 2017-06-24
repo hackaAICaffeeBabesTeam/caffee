679584211eea8a9a2f94714d8a9a8f75b20cc000
Soheil Hamidi <soheilhamidi@me.com>
2017-06-24 17:16:18 -0400

    debug mode on

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from urllib2 import URLError
 from random import randint
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/test', methods=['POST'])
 def test():
@@ -12,7 +13,13 @@ def test():
 
     print(content)
 
-    return json.dumps({'data': 'Hello!'})
+    return json.dumps({
+        "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+        "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+        "data": {},
+        "contextOut": [],
+        "source": "DuckDuckGo"
+        })
 
 @app.route('/get-location', methods=['POST'])
 def getLocation():
