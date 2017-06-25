20dfd1dde91dcfb0c1e468ac0d29aa4dd4dc65ce
Soheil Hamidi <soheilhamidi@me.com>
2017-06-25 15:24:08 -0400

    Bug fix

--- run.py
+++ run.py
@@ -281,5 +281,5 @@ def curl_request(lat, lon, radius, price):
         return jsonify('No API!'), e
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    # app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
+    # app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
